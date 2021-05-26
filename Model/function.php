51014e2b007cdb19f6bf6c060a187903a2d73485
Audric1320 <audricrosier@gmail.com>
2021-05-26 19:46:17 +0200

    Update function.php
    add check pass function

--- Model/function.php
+++ Model/function.php
@@ -23,5 +23,15 @@
 
         return;
     }
+
+    function checkPassword ($pass, $hash){
+        if (password_verify($pass, $hash)) {
+            return true;
+        }
+        
+        else {
+            return false;
+        }
+    }
     
 ?>
