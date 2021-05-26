fafa2b822bf8cc54048d1f0ceff4c2e3c65708d0
Audric1320 <audricrosier@gmail.com>
2021-05-27 00:55:19 +0200

    Update admin.php
    add table !

--- View/admin.php
+++ View/admin.php
@@ -14,6 +14,8 @@
   /* Check if you're connected or not */
     if($AdminId = 0){
       $error = 'NON CONNECTER EN AMDIN';
+
+      end:
       header("Location: ../view/$path?error=$error");
     }
 
@@ -35,6 +37,30 @@
   </head>
 
   <body>
-    
+    <table border = 1>
+      <thead>
+          <tr>
+              <?php foreach ($staff[0] as $key => $value) {
+                if($key != 'Staff_ID'){ ?>
+              <th><?php echo $key ;}?></th>
+              <?php } ?>
+              <th> Update </th>
+              <th> Delete </th>
+          </tr>
+      </thead>
+
+      <tbody>
+        <?php foreach ($staff as $sta) {?>
+          <tr>
+              <?php foreach ($sta as $key2 => $val):
+                if($key2 != 'Staff_ID'){ ?>
+                <td><?php echo $val;}?></td>
+              <?php endforeach; ?>
+              <td> <button type="submit"> <a href="updateUti?pk=<?php echo $sta['Staff_ID'];?>">ChangeMwa!</a></button></td>
+              <td> <button type="submit"> <a href="../controller/deleteUti?pk=<?php echo $sta['Staff_ID'];?>">Foutoncamp!</a></button></td>
+          </tr>
+        <?php }?>
+      </tbody>
+    </table>
   </body>
 </html>
