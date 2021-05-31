85592bcab123b6776e499f67f1c05989516dc02f
Audric_san <audricrosier@gmail.com>
2021-05-31 19:17:27 +0200

    New Admin Backup

---
+++ View/admin2021-05-31.php
@@ -0,0 +1,69 @@
+<?php
+  /* Start Session and Include Read Files */
+    session_start();
+    include('../Model/read.php');
+    include('../Model/function.php');
+
+  /* set error */
+    $error = '';
+    $path = 'index.php';
+
+  /* redefining variables in post */  
+    $AdminId = $_SESSION['Admin_ID'];
+
+  /* Check if you're connected or not */
+    if($AdminId = 0){
+      $error = 'NON CONNECTER EN AMDIN';
+
+      end:
+      header("Location: ../view/$path?error=$error");
+    }
+
+  /* Load All Data For TABLE */
+    $staffid = TakeAllStaffID();
+    $staff = TakeAllStaff();
+
+    foreach ($staffid as $key => $value){
+      $valueInt = intval($value['Staff_ID']);
+      $TAII = TakeAllInfoID($valueInt);
+      var_dump($TAII);
+    }
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+
+  <head>
+    <!--Integration CSS-->
+      <link rel="stylesheet" href="Ellements/CSS/style.css">
+  </head>
+
+  <body>
+    <table border = 1>
+      <thead>
+          <tr>
+              <?php foreach ($TAII[0] as $key => $value) {
+                if($key != 'Staff_ID'){ ?>
+              <th><?php echo $key;}?></th>
+              <?php } ?>
+              <th> Update </th>
+              <th> Delete </th>
+          </tr>
+      </thead>
+
+      <tbody>
+        <?php foreach ($staff as $sta) {?>
+          <tr>
+              <?php foreach ($sta as $key2 => $val):
+                if($key2 != 'Staff_ID'){ ?>
+                <td><?php echo $val; if($val = NULL){echo 'NULL';}}?></td>
+              <?php endforeach; ?>
+              <td> <button type="submit"> <a href="updateUti?pk=<?php echo $sta['Staff_ID'];?>">ChangeMwa!</a></button></td>
+              <td> <button type="submit"> <a href="../controller/deleteUti?pk=<?php echo $sta['Staff_ID'];?>">Foutoncamp!</a></button></td>
+          </tr>
+        <?php }?>
+      </tbody>
+    </table>
+  </body>
+</html>
